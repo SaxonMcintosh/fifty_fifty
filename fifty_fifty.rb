95de020663dc03f8fcf70cdd71e879fa1b4f4965
Saxon McIntosh <saxon.mcintosh@gmail.com>
2018-06-28 12:38:46 -0400

    massive overhaul to script body organization, loops menu, institutes monster counter and visual tweaks

--- fifty_fifty.rb
+++ fifty_fifty.rb
@@ -1,70 +1,79 @@
 # fifty_fifty is a small game that I'm working on to flex my Ruby
 # muscles, but I'm still an absolute novice so I'll be learning a lot
-# as I go on.
-
-# This is essentially functioning as Main, but I'm sure it could be
-# reworked once I know more about what it means to be "idiomatically
-# Ruby-esque.
-class Menu
-  def initialize
-    puts "\nPlease select an option."
-    puts "\nFight\tStore\tSave\tEnd"
-    input
-  end
-
-  def input
-    user_in = gets.chomp
-    user_in.downcase!
-
-# So far, the fight option is the one of gotten to.
-    if user_in == "fight"
-      puts "\nAlright, let\'s go."
-      fight
-    elsif user_in == "store"
-      puts "\nWe'll find some good stuff."
-    elsif user_in == "save"
-      puts "\nSaving this session for you."
-    elsif user_in == "end"
-      puts "\nExiting; it's been a pleasure!"
-    else
-      puts "\nTry typing that again."
-    end
-  end
-
-  # The basic concept is that initially, the player character has a
-  # 50/50 chance of defeating a spawned monster. Eventually, each time
-  # they succeed they will earn a number of points that they can use to 
-  # redeem upgrades, which will stack. Like a freemium phone game sans
-  # real cash.
-  def fight
-    play = Fighter.new
-    play.health = 1
-
-    monst = Fighter.new
-    monst.health = 1
-
-    while play.health > 0 && monst.health > 0 do
-      # This looks unusual presently, but eventually the monsters and
-      # player will have greater health. I also intend to add a monster
-      # counter to help differentiate battles in the console window.
-      puts "\nPlayer HP: #{play.health}\tMonster HP: #{monst.health}."
-      puts "\nAttacking monster!"
-      if rand.round == 1
-        monst.health -= 1
-        puts "\nVictory!"
-        sleep(1)
-        fight
-      else
-        play.health -= 1
-        puts "\nDefeat!"
-      end
-    end
-  end
-end
+# as I go on. The basic concept is that initially, the player character
+# has a 50/50 chance of defeating a spawned monster. Eventually, each
+# time they succeed they will earn a number of points that they can use
+# to redeem upgrades, which will stack. Like a freemium phone game sans
+# real cash.
 
 class Fighter
   attr_accessor :health
   attr_accessor :damage
 end
 
-first = Menu.new
+def fight
+  play = Fighter.new
+  play.health = 1
+    
+  while play.health > 0 do
+    # Instantiates a new monster object, then loops through accumulating
+    # points and producing visual output every time a monster is
+    # defeated. When the player runs out of health, the loop is aborted.
+    # Sleep is instituted to make the display easier to follow.
+    monst = Fighter.new
+    monst.health = 1
+
+    puts "\nHere comes monster ##{$monst_count + 1}!"
+    sleep 0.5
+    puts "\nPlayer HP: #{play.health}\tMonster HP: #{monst.health}"
+    sleep 0.5
+    puts "\nAttacking monster..."
+    sleep 0.5
+    
+    if rand.round == 1
+      monst.health -= 1
+      puts "\nVictory!"
+      $monst_count += 1
+      $points += 1
+      # Eventually this will clog the terminal with splats, but for the
+      # time being it adequately breaks up the display.
+      puts "Bodies left in your wake: #{'*' * $monst_count}"
+      sleep 0.5
+    else
+      play.health -= 1
+      puts "\nDefeat!"
+    end
+  end
+  sleep 1
+  menu
+end
+
+def menu
+  puts "\nPlease select an option."
+  puts "\nFIGHT-f\tSTORE\tSAVE\tEND"
+
+  user_in = gets.chomp
+  user_in.downcase!
+
+  if user_in == "fight" || user_in == "f"
+    puts "\nAlright, let\'s go."
+    fight
+  elsif user_in == "store"
+    puts "\nWe'll find some good stuff."
+  elsif user_in == "save"
+    puts "\nSaving this session for you."
+  elsif user_in == "end"
+    puts "\nExiting; it's been a pleasure!"
+  else
+    puts "\nTry typing that again."
+    menu
+  end
+end
+
+$monst_count = 0
+$points = 0
+
+menu
+
+# Visual output for when I begin writing these variables to file.
+puts $monst_count, $points
